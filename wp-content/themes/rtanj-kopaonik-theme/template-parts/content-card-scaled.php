528cac3296bb9cc2b3bac0f67d476777c9e78cf9
jasna <jas.m.vuk.993@gmail.com>
2025-02-05 22:24:57 +0100

    Adding fields in queries

--- wp-content/themes/rtanj-kopaonik-theme/template-parts/content-card-scaled.php
+++ wp-content/themes/rtanj-kopaonik-theme/template-parts/content-card-scaled.php
@@ -4,5 +4,13 @@
         <h5 class="title"><?php the_title();?></h5>
         <p><?php echo get_the_excerpt();?></p>
         <a href="<?php echo get_the_permalink()?>">Detaljnije</a>
+        <div class="card__bottom">
+            <?php if(!is_main_query()){ ?>
+                <p><i class="fa-solid fa-wallet"></i><?php the_field('cena')?></p>
+                <p><i class="fa-solid fa-calendar-days"></i><?php the_field('broj_dana_nocenja')?></p>
+           <?php } else {?>
+                <p><?php echo get_the_date('d.m.Y');?></p>
+            <?php } ?>
+        </div>
     </div>
 </div>
